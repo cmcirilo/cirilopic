c65b58e89827dd92bd34b4be0d3445f9b0396798
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-10-04 18:27:36 -0300

    implementing not found page

--- frontend/src/app/app.routing.module.ts
+++ frontend/src/app/app.routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'user/cirilo', component: PhotoListComponent },
   { path: 'user/add', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
