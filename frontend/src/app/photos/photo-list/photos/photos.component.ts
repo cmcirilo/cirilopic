37cd82cf0e24f1d88a5d6af4edfdfd644c1d9965
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-11-11 11:21:02 -0300

    implementing photos component

--- frontend/src/app/photos/photo-list/photos/photos.component.ts
+++ frontend/src/app/photos/photo-list/photos/photos.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Photo} from '../../photo/photo';
+import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from '../../photo/photo';
 
 @Component({
   selector: 'app-photos', templateUrl: './photos.component.html', styleUrls: ['./photos.component.css'],
@@ -7,11 +7,22 @@ import {Photo} from '../../photo/photo';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.rows = this.groupColums(this.photos);
   }
 
+  private groupColums(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
 }
