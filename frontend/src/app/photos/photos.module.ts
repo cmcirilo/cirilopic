621ef3ca599539b30bc9968dd2b94a7aa7b5d52c
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-11-28 20:16:32 -0300

    implementing pipe filter

--- frontend/src/app/photos/photos.module.ts
+++ frontend/src/app/photos/photos.module.ts
@@ -5,9 +5,11 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent], imports: [CommonModule, HttpClientModule],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription],
+  imports: [CommonModule, HttpClientModule],
 })
 export class PhotosModule {
 }
