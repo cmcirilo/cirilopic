f53f60f986b1281f3b66eb93e03007a7ec7d1645
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-11-28 22:24:41 -0300

    implementing debounceTime operator

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list', templateUrl: './photo-list.component.html', styleUrls: ['./photo-list.component.css'],
@@ -9,12 +11,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter = '';
+  filterSubject = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.filterSubject
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
 }
