59744c426412768f63ef28058f62a8cf9fca206a
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-11-28 21:57:28 -0300

    implementing resolver

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css'],
+  selector: 'app-photo-list', templateUrl: './photo-list.component.html', styleUrls: ['./photo-list.component.css'],
 })
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
-  filter: string = '';
+  filter = '';
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute,
-  ) {
+  constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe((photos) => (this.photos = photos));
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
 }
