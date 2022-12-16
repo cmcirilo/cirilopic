40c5215069be1ae2dbd295820762c3eb502d6d37
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-12-16 19:17:50 -0300

    implementing form reactive

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/sign-in.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MessageModule } from '../shared/components/message/message.module';
 
 @NgModule({
   declarations: [SignInComponent],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MessageModule,
+  ],
 })
 export class HomeModule {
 
