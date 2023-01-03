6da5e45c73dd487feec1d581b1f0ca68a7e79b3a
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-01-03 20:13:41 -0300

    implementing header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule, HomeModule],
+  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule, HomeModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
