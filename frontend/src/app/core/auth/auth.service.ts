bff45d87a9123693eaefce3b4cb735dfa7572938
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-01-04 15:53:28 -0300

    implementing user service

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -10,7 +10,7 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient, private tokenService: TokenService) {
+  constructor(private httpClient: HttpClient, private userService: UserService) {
   }
 
   authenticate(userName: string, password: string) {
@@ -19,7 +19,7 @@ export class AuthService {
       { observe: 'response' })
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
         console.log(`User ${userName} authenticated with token ${authToken}`);
       }));
   }
