1749bba6f9c8a93ff2bf00044eaa79b0662eae08
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-01-05 11:49:24 -0300

    implementing signup

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/sign-in.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MessageModule } from '../shared/components/message/message.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [SignInComponent, SignupComponent],
   imports: [
     ReactiveFormsModule,
+    FormsModule,
     CommonModule,
     MessageModule,
     RouterModule,
