0670cc50c7f09ecbd2249a266c95bd9e8e6b50ae
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-01-06 11:00:54 -0300

    implementing lowercase validator

--- frontend/src/app/home/signup/signup.component.ts
+++ frontend/src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from '../../shared/validators/lower-case.validator';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -15,7 +16,7 @@ export class SignupComponent implements OnInit {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-      userName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40), Validators.pattern(/^[a-z0-9_\-]+$/)]],
+      userName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40), lowerCaseValidator]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     });
   }
