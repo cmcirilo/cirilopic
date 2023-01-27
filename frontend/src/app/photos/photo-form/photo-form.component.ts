769734c0b30b04cc068cdc50bd58000076fe5ad2
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-01-27 11:38:55 -0300

    implementing photo form

--- frontend/src/app/photos/photo-form/photo-form.component.ts
+++ frontend/src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+  styleUrls: ['./photo-form.component.css'],
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  photoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
 
 }
--- frontend/src/app/photos/photo-form/photo-form.module.ts
+++ frontend/src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MessageModule } from '../../shared/components/message/message.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MessageModule, FormsModule,
+  ],
 })
 export class PhotoFormModule {
 }
