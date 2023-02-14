a4279b3b51efae5e32cc5efb54c36798bdc52cb0
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-02-14 10:50:24 -0300

    styling photo upload

--- frontend/src/app/photos/photo-form/photo-form.module.ts
+++ frontend/src/app/photos/photo-form/photo-form.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MessageModule } from '../../shared/components/message/message.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, MessageModule, FormsModule, RouterModule,
+  imports: [CommonModule, ReactiveFormsModule, MessageModule, FormsModule, RouterModule, PhotoModule,
   ],
 })
 export class PhotoFormModule {
