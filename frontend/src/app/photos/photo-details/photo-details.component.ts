45c0073a0d4786c857df6aa09855bc9bdf529447
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-03-30 09:19:30 -0300

    implementing photo comments

--- frontend/src/app/photos/photo-details/photo-details.component.ts
+++ frontend/src/app/photos/photo-details/photo-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 import { Observable } from 'rxjs';
+import { PhotoComment } from '../photo/photo-comment';
 
 @Component({
   templateUrl: './photo-details.component.html',
@@ -11,11 +12,14 @@ import { Observable } from 'rxjs';
 export class PhotoDetailsComponent implements OnInit {
 
   photo$: Observable<Photo>;
+  comments$: Observable<PhotoComment[]>;
 
   constructor(private router: ActivatedRoute, private photoService: PhotoService) {
   }
 
   ngOnInit() {
-    this.photo$ = this.photoService.findById(this.router.snapshot.params.photoId);
+    const photoId = this.router.snapshot.params.photoId;
+    this.photo$ = this.photoService.findById(photoId);
+    this.comments$ = this.photoService.getComments(photoId);
   }
 }
