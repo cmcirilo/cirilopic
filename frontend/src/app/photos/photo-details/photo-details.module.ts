2fafdffe3c7fa63b96699719ad30776f52761715
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2023-04-02 17:44:50 -0300

    implementing visibility for comments

--- frontend/src/app/photos/photo-details/photo-details.module.ts
+++ frontend/src/app/photos/photo-details/photo-details.module.ts
@@ -6,9 +6,10 @@ import { PhotoCommentsComponent } from './photo-comments/photo-comments.componen
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MessageModule } from '../../shared/components/message/message.module';
+import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
 
 @NgModule({
-  declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
+  declarations: [PhotoDetailsComponent, PhotoCommentsComponent, PhotoOwnerOnlyDirective],
   exports: [PhotoDetailsComponent, PhotoCommentsComponent],
   imports: [CommonModule, PhotoModule, RouterModule, ReactiveFormsModule, MessageModule],
 })
